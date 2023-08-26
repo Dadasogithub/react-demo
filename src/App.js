be357b1c72c4368d705435649a4f1106d3ff1c78
dadaso <101014255+Dadasogithub@users.noreply.github.com>
2023-08-26 16:03:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome <code> Live</code> Website
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React and Deploy Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
